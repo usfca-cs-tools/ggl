853e384c4d0a1f1343a09ab77919cee5f0c35793
spganti <spanuganti@dons.usfca.edu>
2025-07-02 21:04:10 -0700

    added unit tests for bitwise gates, not gate is not working correctly yet

---
+++ tests/ggl/1-not.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('../')
+
+from ggl import circuit
+from ggl import io
+from ggl import logic
+
+c = circuit.Circuit()
+g = logic.Not(bits=1, label='r')
+
+a = io.Input(bits=1, label='a')
+a.value = 0b1
+c.connect(a, g.input("0"))
+
+r = io.Output(bits=1, label='r')
+c.connect(g, r)
+
+c.run()
+print(r.value)
---
+++ tests/ggl/1-xor-1.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('../')
+
+from ggl import circuit
+from ggl import io
+from ggl import logic
+
+c = circuit.Circuit()
+g = logic.Xor(bits=1, label='r')
+
+a = io.Input(bits=1, label='a')
+a.value = 0b1
+c.connect(a, g.input("0"))
+
+b = io.Input(bits=1, label='b')
+b.value = 0b1
+c.connect(b, g.input("1"))
+
+r = io.Output(bits=1, label='r')
+c.connect(g, r)
+
+c.run()
+print(r.value)
