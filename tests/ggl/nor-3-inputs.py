5ac43e10e66fedfa8bc3628d18f38427ae4c2dea
spganti <spanuganti@dons.usfca.edu>
2025-07-02 22:58:56 -0700

    xor works, however the not functions don't work because of intermediate results and applying not before other operators

--- tests/ggl/nor-3-inputs.py
+++ tests/ggl/nor-3-inputs.py
@@ -14,7 +14,7 @@ b.value = 0x0F                              # 0b00001111 15
 c = io.Input(bits=8, label="C")
 c.value = 60                                # 0b00111100
 
-nor1 = logic.Nor(num_inputs=3)
+nor1 = logic.Nor(num_inputs=3, bits=8)
 circuit0.connect(a, nor1.input("0"))
 circuit0.connect(b, nor1.input("1"))
 circuit0.connect(c, nor1.input("2"))
@@ -22,5 +22,5 @@ circuit0.connect(c, nor1.input("2"))
 r = io.Output(bits=4, label="R")
 circuit0.connect(nor1, r)
 
-circuit0.run()                              # output should be 63/ 0b11111111
+circuit0.run()                              # output should be 0/ 0b0
 print(r.value) 
