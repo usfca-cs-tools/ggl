64269ad0f2840a7c4dd21fc71ffad9400df9fc0d
spganti <spanuganti@dons.usfca.edu>
2025-07-14 12:44:45 -0700

    added simple 32 bit test for splitter and merger

---
+++ tests/ggl/32bit-splitter-merger.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('../')
+
+from ggl import circuit
+from ggl import io
+from ggl import wires
+
+circuit0 = circuit.Circuit()
+
+
+a = io.Input(bits=32, label="a")
+a.value = 0x12345678 
+
+split = wires.Splitter(label="split", bits=32, splits=[8, 8, 8, 8])
+
+
+merge = wires.Merger(label="merge", bits=32, merge_inputs=[8, 8, 8, 8])
+
+
+r = io.Output(bits=32, label="r")
+circuit0.connect(a, split.inputs[0])
+
+for i in range(len(split.splits)):
+    circuit0.connect(split.outputs[i], merge.inputs[i])
+
+circuit0.connect(merge, r.inputs[0])
+circuit0.run()
+
+print(r.value)
