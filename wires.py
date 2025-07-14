5f517a28c1ceb67d9b24f2f18a44f646d51acf3d
spganti <spanuganti@dons.usfca.edu>
2025-07-14 12:35:36 -0700

    edited to take in list of split/merge sizes

--- wires.py
+++ wires.py
@@ -26,8 +26,12 @@ class Splitter(WireNode):
     """
     kind = 'Splitter'
     
-    def __init__(self, label='', bits=1):
-        super().__init__(Splitter.kind, num_inputs=1, num_outputs=bits, label=label, bits=bits)
+    def __init__(self, label='', bits=1, splits=None):
+        if splits is None:
+            splits = [1] * bits                                         # default to 1-bit outputs
+        # TODO: error checking if the splits don't add up to total input bits
+        self.splits = splits
+        super().__init__(Splitter.kind, num_inputs=1, num_outputs=len(splits), label=label, bits=bits)
 
     def propagate(self, value=0):
         # TODO: edit so that it can be customized to more than singular bit outputs (ex: 32 bits -> 8, 8, 8, 8 bit outputs)
@@ -35,15 +39,17 @@ class Splitter(WireNode):
         input_value = input_edge.value if input_edge else 0
 
         new_work = []
+        offset = 0
+        for i, width in enumerate(self.splits):
+            mask = (1 << width) - 1                                     # mask bit at i to get bit_val, and then propagate to the output node
+            chunk = (input_value >> offset) & mask
+            logger.info(f'{self.kind} {self.label} output {i}: {bin(chunk)}')
 
-        for i in range(self.bits):
-            # mask bit at i to get bit_val, and then propagate to the output node
-            bit_val = (input_value >> i) & 1
-            logger.info(f'{self.kind} {self.label} output {i}: {bit_val}')
-            
             for edge in self.outputs.points[str(i)]:
-                edge.propagate(bit_val)
+                edge.propagate(chunk)
                 new_work += edge.get_dest_nodes()
+
+            offset += width
 
         return new_work
 
@@ -53,17 +59,26 @@ class Merger(WireNode):
     """
     kind = 'Merger'
     
-    def __init__(self, label='', bits=1):
-        super().__init__(Merger.kind, num_inputs=bits, num_outputs=1, label=label, bits=bits)
+    def __init__(self, label='', bits=1, merge_inputs=None):
+        if merge_inputs is None:
+            merge_inputs = [1] * bits                                   # default to 1-bit inputs
+
+        self.merge_inputs = merge_inputs
+        super().__init__(Merger.kind, num_inputs=len(merge_inputs), num_outputs=1, label=label, bits=bits)
 
     def propagate(self, value=0):
-        bits = 0
-        for i, edge in enumerate(self.inputs.get_edges()):
-            bit = edge.value & 1
-            # or all bits together to get an output value
-            bits |= (bit << i)
-        logger.info(f'{self.kind} {self.label} combined value: {bits}')
-        return super().propagate(bits)
+        output_val = 0
+        offset = 0
+
+        for i, width in enumerate(self.merge_inputs):
+            edge = self.inputs.get_edge(str(i))
+            input_val = edge.value if edge else 0                       # mask to width and shift into position
+            chunk = input_val & ((1 << width) - 1)
+            output_val |= (chunk << offset)                             # or all bits together to get an output value
+            offset += width
+
+        logger.info(f'{self.kind} {self.label} combined value: {bin(output_val)}')
+        return super().propagate(output_val)
     
 class Tunnel(WireNode):
     """
